d529ece53949cd29c521eacdb0710abf689e410b
Kuropatka-chan <nositel17@mail.ru>
2025-12-15 01:15:09 +0300

    Create visual novel prototype and scenario files

---
+++ assets.js
@@ -0,0 +1,29 @@
+// Файл отвечает за сопоставление логических названий сцен и спрайтов с фактическими текстурами.
+// Текстуры можно положить рядом и обновить пути, сохранив те же ключи.
+
+export const backgrounds = {
+  UpperTown1: 'textures/UpperTown1.png',
+  LowerTown1: 'textures/LowerTown1.png',
+  Home1: 'textures/Home1.png',
+  LowerTown2: 'textures/LowerTown2.png',
+  Jobtown: 'textures/Jobtown.png',
+  Podzalupie: 'textures/Podzalupie.png',
+};
+
+export const sprites = {
+  Osvald1: 'textures/Osvald1.png',
+  Osvald2: 'textures/Osvald2.png',
+  Osvald3: 'textures/Osvald3.png',
+  Osvald4: 'textures/Osvald4.png',
+  Mom1: 'textures/Mom1.png',
+  Karl1: 'textures/Karl1.png',
+  Dryg1: 'textures/Dryg1.png',
+  Dryg2: 'textures/Dryg2.png',
+  loh: 'textures/loh.png',
+  npc1: 'textures/npc1.png',
+  war1: 'textures/war1.png',
+  Shmir1: 'textures/Shmir1.png',
+  Laur1: 'textures/Laur1.png',
+  Ganud1: 'textures/Ganud1.png',
+  Ganud2: 'textures/Ganud2.png',
+};
